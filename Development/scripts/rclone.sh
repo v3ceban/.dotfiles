fc941c8ad1905eda97b3fa12569f896043e61afc
Vladimir Ceban <v3ceban@proton.me>
2025-07-06 23:45:19 -0700

    upd: remove scripts

--- Development/scripts/rclone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# rclone mount "Dropbox:/" "$HOME/Cloud/Dropbox" --daemon --vfs-cache-mode full &
-rclone mount "Proton:/" "$HOME/Cloud/Proton" --daemon --vfs-cache-mode full &
-# rclone mount "Google Drive:/" "$HOME/Cloud/Google Drive" --daemon --vfs-cache-mode full &
