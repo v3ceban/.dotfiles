1636f584e70b2b7975934440504997694152906f
Vladimir Ceban <v3ceban@proton.me>
2025-08-11 14:38:13 -0700

    fix(eslint_d): ignore files in

--- .config/nvim/lua/configs/lint.lua
+++ .config/nvim/lua/configs/lint.lua
@@ -81,6 +81,9 @@ lint.linters_by_ft = {
 vim.o.updatetime = 200
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "CursorHold" }, {
   callback = function()
-    lint.try_lint()
+    local file = vim.api.nvim_buf_get_name(0)
+    if not file:match("node_modules") then
+      lint.try_lint()
+    end
   end,
 })
