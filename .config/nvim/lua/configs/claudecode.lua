db9d5d6f0f4db6a3196f8bee0e13d78df4ac4ccd
Vladimir Ceban <v3ceban@proton.me>
2026-02-13 14:58:25 -0800

    feat(claude): fixed binary path and nvim integration attempt

--- .config/nvim/lua/configs/claudecode.lua
+++ .config/nvim/lua/configs/claudecode.lua
@@ -2,15 +2,26 @@ local M = {}
 
 M.opts = {
   terminal = {
-    provider = "none",
+    provider = "snacks", -- "auto", "snacks", "native", "external", "none"
+    split_width_percentage = 0.4,
+    snacks_win_opts = {
+      -- https://github.com/folke/snacks.nvim/blob/main/docs/win.md
+      position = "right",
+      height = 0.4,
+      width = 0.4,
+      wo = { winbar = "" },
+      bo = { filetype = "claude_code" },
+    },
   },
   diff_opts = {
-    open_in_current_tab = false,
+    open_in_new_tab = true,
     hide_terminal_in_new_tab = true,
+    on_new_file_reject = "close_window",
   },
 }
 
 M.keys = {
+  { "<M-a>", "<cmd>ClaudeCode<cr>", mode = { "n", "t" }, desc = "AI toggle claude terminal" },
   { "<leader>aa", "<cmd>ClaudeCodeAdd %<cr>", mode = "n", desc = "AI send file to claude" },
   { "<leader>aa", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "AI send selection to claude" },
   {
@@ -22,6 +33,17 @@ M.keys = {
   },
   { "<leader>ay", "<cmd>ClaudeCodeDiffAccept<cr>", mode = "n", desc = "AI accept claude change" },
   { "<leader>an", "<cmd>ClaudeCodeDiffDeny<cr>", mode = "n", desc = "AI deny claude change" },
+  { "<C-h>", "<cmd>wincmd h<cr>", mode = "t", ft = "claude_code" },
+  {
+    "<C-n>",
+    function()
+      vim.cmd "ClaudeCode"
+      vim.cmd "NvimTreeToggle"
+      vim.cmd "ClaudeCode"
+    end,
+    mode = "t",
+    ft = "claude_code",
+  },
 }
 
 return M
