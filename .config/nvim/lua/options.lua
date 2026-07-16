f26b84d4f93145ac3411032bf9cd37dc54a0d0a0
Vladimir Ceban <v3ceban@proton.me>
2026-07-15 22:58:54 -0700

    fix(tmux): disable vim navigation wrap

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -9,6 +9,7 @@ vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 99
 vim.g.markdown_recommended_style = 0
 vim.g.copilot_no_tab_map = true
+vim.g.tmux_navigator_no_wrap = 1
 vim.opt.completeopt = "menu,menuone,noselect,popup"
 vim.opt.laststatus = 3
 vim.opt.clipboard = "unnamedplus"
